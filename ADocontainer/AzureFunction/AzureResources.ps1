6ce2994d6ba9ab5387b240eadaca46ce13dc1977
Gabriel McColl <gabriel@cloudkingdoms.com>
2020-07-04 23:17:52 +0100

    gave own account permissions to key vault

--- ADocontainer/AzureFunction/AzureResources.ps1
+++ ADocontainer/AzureFunction/AzureResources.ps1
@@ -16,3 +16,6 @@ New-AzFunctionApp -ResourceGroupName $ResourceGroupName -Location $location -OST
 #Give the Function App permission to get Secrets from the Azure Key Vault we just made. 
 $id = (Get-AzFunctionApp -Name $FunctionAppName -ResourceGroupName $ResourceGroupName).IdentityPrincipalId
 Set-AzKeyVaultAccessPolicy -VaultName $keyVaultname -ObjectId $id -PermissionsToSecrets get -BypassObjectIdValidation
+##Give your own account permissions to see stuff on the keyvault
+$id = $env:ACC_OID
+Set-AzKeyVaultAccessPolicy -VaultName $keyVaultname -ObjectId $id -PermissionsToSecrets list,get,set,delete -BypassObjectIdValidation
