37f290242dbee96334befabcbd7a8001eb258e20
Gabriel McColl <gabriel@cloudkingdoms.com>
2020-07-05 11:45:26 +0100

    added in variables for keyvault

--- ADocontainer/AzureFunction/run.ps1
+++ ADocontainer/AzureFunction/run.ps1
@@ -5,10 +5,30 @@ param($Request, $TriggerMetadata)
 
 #Check if a build is queued before launching a container
 #You need to put the key in keyvault
-$buildapikey = (Get-AzKeyVaultSecret -VaultName adocontainer -Name GetBuilds).SecretValueText
+$VaultName = 'ContainerAgentKeyjk'
+$buildapikey = (Get-AzKeyVaultSecret -VaultName $VaultName -Name GetBuilds).SecretValueText
 $ADOOrganization = 'yourorg'
-$ADOProject = 'Project'
+$ADOProject = 'yourproject'
+$resourcegroupname = 'ContainerAgentJK'
+#You don't really need the body anymore, was left over from the sample but it shows how this could be worked on more.
+$body = "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
 
+#Make the name unique for the registered agent
+$rand = Get-Random -Maximum 1000000
+$agentname = 'dockerlinuxjek' + $rand
+
+#environmental variables for the docker container to use once it's spun up
+$dockerenv = @{
+    AZP_URL="Https://dev.azure.com/$ADOOrganization/" ;
+    AZP_AGENT_NAME=$agentname ;
+    AZP_POOL='Jekyll';
+    $AZ_SECRET_NAME='the Name of the secret. Not the Value.';
+    $AZ_KEY_VAULT='Name of the Key Vault'
+} 
+
+$containerinstancename = 'jekyllcontainerado2'
+
+#the API token needs to be in base64 to use to invoke the web request
 function Test-Build {
     $url = "https://dev.azure.com/$ADOOrganization/$ADOProject/_apis/build/builds?statusFilter=notstarted&api-version=5.1"
     $token = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes(":$buildapikey"))
@@ -32,32 +52,20 @@ do {
     }
 } until ($null -ne $buildqueued)
 
-# Interact with query parameters or the body of the request.
-$name = 'jekyllcontainerado'
+# Interact with query param2eters or the body of the request.
+#this will be the name of the Azure Container Instance
 
-$rand = Get-Random -Maximum 1000000
-#You don't really need some of this anymore, was left over from the sample but it shows how this could be worked on more.
-$body = "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-$agentname = 'dockerlinuxjek' + $rand
-$resourcegroupname = 'ContainerAgent'
-
-$dockerenv = @{
-    AZP_URL="Https://dev.azure.com/$ADOOrganization/" ;
-    AZP_AGENT_NAME=$agentname ;
-    AZP_POOL='Jekyll'
-} 
-
-if ($name) {
-    New-AzContainerGroup -ResourceGroupName $resourcegroupname -Name $name `
+if ($containerinstancename) {
+    New-AzContainerGroup -ResourceGroupName $resourcegroupname -Name $containerinstancename `
         -Image gabrielmccoll/jekylladoagentminmistakes:latest -OsType linux `
         -RestartPolicy Never -EnvironmentVariable $dockerenv -AssignIdentity
-    $body = "Started container group $name"
+    $body = "Started container group $containerinstancename"
 }
 
 #The first time the container group is made, this part might take a couple mins to propograte.
 #Meaning your container will fail with an error message about not being able to access the key. 
-$id = (Get-AzContainerGroup -ResourceGroupName $resourcegroupname -Name $name).Identity.PrincipalId
-Set-AzKeyVaultAccessPolicy -VaultName adocontainer -ObjectId $id -PermissionsToSecrets get -BypassObjectIdValidation
+$id = (Get-AzContainerGroup -ResourceGroupName $resourcegroupname -Name $containerinstancename).Identity.PrincipalId
+Set-AzKeyVaultAccessPolicy -VaultName $VaultName -ObjectId $id -PermissionsToSecrets get -BypassObjectIdValidation
 
 # Associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
